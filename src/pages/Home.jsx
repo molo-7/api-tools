ed28def3e1f765dd2568e250409dddb61c138824
MOLO-7 <am8736419@gmail.com>
2022-07-03 14:08:43 +0200

    remove unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import apiIcon from "../images/api.png";
 import Modal from "../components/Builders/Modal";
