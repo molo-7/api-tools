cb84ef3b3a09a3c4df81a0405c5f69fc1ad73ebe
MOLO-7 <am8736419@gmail.com>
2022-07-03 13:58:44 +0200

    create tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      titlebar: "#141414",
+      dark: "#0e0e0e",
+      black: "#000",
+      blue: "#4c6ef5",
+      white: {
+        900: "rgb(255, 255, 255)",
+        700: "rgba(255, 255, 255, 0.75)",
+        500: "rgba(255, 255, 255, 0.5)",
+        300: "rgba(255, 255, 255, 0.3)",
+        200: "rgba(255, 255, 255, 0.2)",
+      },
+      buttons: {
+        primary: "#5865f2",
+        secondary: "#1b1b1d",
+        tertiary: "#4d4c7d",
+        succes: "#43b581",
+      },
+      danger: "#f04747",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
